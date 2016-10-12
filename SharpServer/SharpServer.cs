0244570952a7990845b56c6eec8d4a7732944102
Frederik Gelder <frederik.gelder@freenet.de>
2016-10-12 14:41:51 +0200

    initial

---
+++ SharpServer/SharpServer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SharpServer
+{
+    public class SharpServer
+    {
+        HttpListener listener = new HttpListener();
+        Thread listenerThread;
+
+        void Start()
+        {
+            listener.Start();
+            listenerThread = new Thread(Listen);
+            listenerThread.Start();
+        }
+
+        void Stop()
+        {
+            listener.Stop();
+            listenerThread.Interrupt();
+        }
+
+        void Listen()
+        {
+            while(listener.IsListening)
+            {
+                try
+                {
+                    HttpListenerContext context = listener.GetContext();
+                    ThreadPool.QueueUserWorkItem(Process, context);
+                }
+                catch(ThreadInterruptedException)
+                {
+                    break;
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    break;
+                }
+            }
+        }
+
+        void Process(object argument) {
+            HttpListenerContext context = (HttpListenerContext)argument;
+            //parse request and find file for it
+            //or should we try dynamic loading and unloading of assemblies again?
+        }
+    }
+}
---
+++ SharpServer/SiteConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpServer
+{
+    public class SiteConfig
+    {
+        public DirectoryInfo FileRootDir { get; set; }
+
+
+        public SiteConfig(FileInfo siteConfig)
+        {
+
+        }
+    }
+}
